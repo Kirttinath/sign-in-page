8f8f3e2d280a54d317e3af7a52e1448e87d9a9f9
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-13 14:34:43 +0530

    Add login and signup form

--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -1,12 +1,41 @@
 import React from 'react'
 import "./LoginSignup.css";
+import email_icon from "../Assets/mail.png";
+import pass_icon from "../Assets/pass.png";
+import user_icon from "../Assets/user.png";
 
- const LoginSignup = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+
+
+const LoginSignup = () => {
+    return (
+        <div className='container'>
+            <div className="header">
+                <div className="text">
+                    <h1>Sign-Up</h1>
+                </div>
+                <div className="inputs">
+                    <div className="input">
+                        <img src={user_icon} alt="" />
+                        <input type="text" />
+                    </div>
+                    <div className="input">
+                        <img src={email_icon} alt="" />
+                        <input type="email" required />
+                    </div>
+                    <div className="input">
+                        <img src={pass_icon} alt="" />
+                        <input type="password" required />
+                    </div>
+                </div>
+                <div className="forgot-password">Forgot Password ?<span>Click Here!</span></div>
+                <div className="submit-container">
+                    <div className="submit">Sign Up</div>
+                    <div className="submit">Log In</div>
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
 export default LoginSignup
